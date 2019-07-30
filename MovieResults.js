c79fc0a3d8dd571d28f1721e6b933cd070cd8b21
Rodriguez-Harrison <rodriguq@wwt.com>
2019-07-30 16:02:39 -0500

    At passing state of tests

---
+++ MovieResults.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {
+  Text,
+} from 'react-native';
+
+export default class MovieResults extends Component {
+  render() {
+    return (
+      <Text>Movie List??</Text>
+    );
+  }
+}
---
+++ Routes.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import { Router, Scene } from 'react-native-router-flux'
+// import SearchPage from './SearchPage'
+// import MovieResults from './MovieResults'
+
+// const Routes = () => (
+//    <Router>
+//       <Scene key = "root">
+//          <Scene key = "search" component = {SearchPage} title = "Search" initial = {true} />
+//          <Scene key = "movielist" component = {MovieResults} title = "Movies" />
+//       </Scene>
+//    </Router>
+// )
+// export default Routes
